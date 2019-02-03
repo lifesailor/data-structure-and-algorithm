9d84dedacdfd8b5a2303a1dba5b8ee6ef1eace41
lifesailor <sailyourlife91@gmail.com>
2019-02-03 21:31:29 +0900

    resize stack array

---
+++ chapter1/1.stack/resizestackarray-client.py
@@ -0,0 +1,25 @@
+from resizestackarray import ResizeStackArray
+
+
+class Client:
+    """
+    FixedCapcityStack client
+    """
+    def test(self, args):
+        stack = ResizeStackArray()
+
+        while args:
+            item = args.pop(0)
+            if item is not "-":
+                stack.push(item)
+            elif not stack.is_empty():
+                print(stack.pop(), end=' ')
+        print("(" + str(stack.size()) + " left on the stack" + ")")
+
+
+if __name__ == "__main__":
+    stack_client = Client()
+    words =['to', 'be', 'or', 'not', 'to', '-', 'be', '-', '-', 'that', '-', '-', '-', 'is']
+    stack_client.test(words)
+
+
---
+++ chapter1/1.stack/resizestackarray.py
@@ -0,0 +1,52 @@
+class ResizeStackArray:
+    """
+    - ResizeStackArray: 빈 스택 생성
+    - void push(Item): 항목 추가
+    - Item pop(): 가장 최근에 추가된 항목 제거
+    - boolean is_empty(): 스택이 비었는가?
+    - int size(): 스택에 든 항목 개수
+    - void resize(): 스택 크기 변경
+    """
+    def __init__(self):
+        self.item_ary = []
+        self.n = 0
+
+    def is_empty(self):
+        if self.n == 0:
+            return True
+        else:
+            return False
+
+    def size(self):
+        return self.n
+
+    def push(self, item):
+        if self.n == len(self.item_ary):
+            self.resize(self.n * 2 + 1)
+        self.item_ary[self.n] = item
+        self.n += 1
+
+    def resize(self, max):
+        item_ary = [None] * max
+        for i in range(self.n):
+            item_ary[i] = self.item_ary[i]
+        self.item_ary = item_ary
+
+    def pop(self):
+        assert self.n > 0
+        self.n -= 1
+
+        ele = self.item_ary[self.n]
+        self.item_ary[self.n] = None
+
+        if self.n == len(self.item_ary) // 4:
+            self.resize(len(self.item_ary) // 2)
+        return ele
+
+
+
+
+
+
+
+
