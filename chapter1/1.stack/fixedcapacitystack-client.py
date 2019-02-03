ac73d04584720409006a0076817cf05f5f9fe7b4
lifesailor <sailyourlife91@gmail.com>
2019-02-03 21:31:11 +0900

    fixed capacity stack

---
+++ chapter1/1.stack/fixedcapacitystack-client.py
@@ -0,0 +1,25 @@
+from fixedcapacitystack import FixedCapacityStack
+
+
+class Client:
+    """
+    FixedCapcityStack client
+    """
+    def test(self, args):
+        stack = FixedCapacityStack(100)
+
+        while args:
+            item = args.pop(0)
+            if item is not "-":
+                stack.push(item)
+            elif not stack.is_empty():
+                print(stack.pop(), end=' ')
+        print("(" + str(stack.size()) + " left on the stack" + ")")
+
+
+if __name__ == "__main__":
+    stack_client = Client()
+    words =['to', 'be', 'or', 'not', 'to', '-', 'be', '-', '-', 'that', '-', '-', '-', 'is']
+    stack_client.test(words)
+
+
---
+++ chapter1/1.stack/fixedcapacitystack.py
@@ -0,0 +1,41 @@
+class FixedCapacityStack:
+    """
+    - FixedCapacityStackOfStrings(int cap) cap을 용량으로 하는 빈 스택 생성
+    - void push(Item): 항목 추가
+    - Item pop(): 가장 최근에 추가된 항목 제거
+    - boolean is_empty(): 스택이 비었는가?
+    - int size(): 스택에 든 항목 개수
+    """
+
+    def __init__(self, cap):
+        """
+        item_ary: fixed size array
+        n: number of elements in array
+
+        :param cap:
+        """
+        self.item_ary = [None] * cap
+        self.n = 0
+
+    def is_empty(self):
+        if self.n == 0:
+            return True
+        else:
+            return False
+
+    def size(self):
+        return self.n
+
+    def push(self, item):
+        assert self.n <= len(self.item_ary) - 1  # item_ary 가 꽉 차있으면 에러 출력
+
+        self.item_ary[self.n] = item
+        self.n += 1
+
+    def pop(self):
+        assert self.n > 0  # 비어 있으면 에러 출력
+
+        self.n -= 1
+        ele = self.item_ary[self.n]
+        self.item_ary[self.n] = None
+        return ele
