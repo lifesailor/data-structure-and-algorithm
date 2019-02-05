57f69471428cd428206eca3a9ec1eb8065fa3df7
lifesailor <sailyourlife91@gmail.com>
2019-02-05 23:31:43 +0900

    sort compare

--- chapter2/sort_compare.py
+++ chapter2/sort_compare.py
@@ -3,6 +3,8 @@ import random
 from selection_sort.selection_sort import SelectionSort
 from insertion_sort.insertion_sort import InsertionSort
 from shell_sort.shell_sort import ShellSort
+from merge_sort.top_down_merge_sort import MergeSort
+from quick_sort.quick_sort import QuickSort
 
 
 class SortCompare:
@@ -16,6 +18,10 @@ class SortCompare:
             InsertionSort.sort(ary)
         elif alg == "Shell":
             ShellSort.sort(ary)
+        elif alg == "Merge":
+            MergeSort.sort(ary)
+        elif alg == "Quick":
+            QuickSort.sort(ary)
         end_time = time.time()
         return end_time - start_time
 
@@ -35,12 +41,14 @@ if __name__ == "__main__":
     alg1 = "Insertion"
     alg2 = "Selection"
     alg3 = "Shell"
+    alg4 = "Merge"
+    alg5 = "Quick"
     N = 1000
     T = 10
 
-    t1 = SortCompare.time_random_input(alg1, N, T)
+    t1 = SortCompare.time_random_input(alg5, N, T)
     t2 = SortCompare.time_random_input(alg3, N, T)
 
     print("For {} random doubles".format(N))
-    print("{0} is {2} times faster than {1}".format(alg1, alg3, t2/t1))
+    print("{0} is {2} times faster than {1}".format(alg5, alg3, t2/t1))
 
